ab896ee08ba85dbd7a39d3a2321b47d02ca49fd6
Jim Irrer <irrer@umich.edu>
2018-04-18 11:49:05 -0400

    Added display for test.

--- src/test/java/edu/umro/ImageUtil/test/TestFindWorstPixels.java
+++ src/test/java/edu/umro/ImageUtil/test/TestFindWorstPixels.java
@@ -8,22 +8,14 @@ import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
 import io.scif.img.SCIFIOImgPlus;
 import net.imglib2.Cursor;
-import net.imglib2.Interval;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.neighborhood.Neighborhood;
-import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.algorithm.region.hypersphere.HyperSphere;
 import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
-import net.imglib2.img.ImgFactory;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.UnsignedShortType;
-import net.imglib2.util.Intervals;
-import net.imglib2.view.Views;
 
 /**
  * Find the pixels whose value is most different from their neighbors and
@@ -36,19 +28,40 @@ public class TestFindWorstPixels {
 		// open with ImgOpener
 		String fileName = "src\\test\\resources\\TestFindWorstPixels.dcm";
 
-		// Img<T> img = (Img<T>) new ImgOpener().openImg(fileName);
+		@SuppressWarnings("unchecked")
 		SCIFIOImgPlus<T> img = (SCIFIOImgPlus<T>) (new ImgOpener().openImgs(fileName).get(0));
 
-		TreeMap<Float, Cursor<T>> worst = FindWorstPixels.findWorstPixels(img, 20);
+		TreeMap<Float, Cursor<T>> worst = FindWorstPixels.findWorstPixels(img, 2);
+
+		boolean onOff = false;
 
 		for (float bad : worst.keySet()) {
 			Cursor<T> cursor = worst.get(bad);
 			System.out.println("    " + bad + " : " + cursor.getIntPosition(0) + ", " + cursor.getIntPosition(1));
+			HyperSphere<T> inner = new HyperSphere<T>(img, cursor, 6);
+			HyperSphere<T> outer = new HyperSphere<T>(img, cursor, 9);
+
+			for (T out : outer) {
+				boolean markIt = true;
+				for (T in : inner) {
+					if (in.compareTo(out) == 0) {
+						markIt = false;
+						break;
+					}
+				}
+				if (markIt) {
+					if (onOff) {
+						out.setReal(50000);
+					} else {
+						out.setReal(0);
+					}
+					onOff = !onOff;
+				}
+			}
 		}
 
-		// display output and input
+		// display input
 		ImageJFunctions.show(img);
-		// ImageJFunctions.show(display);
 	}
 
 	public static void main(String[] args) throws ImgIOException, IncompatibleTypeException {
