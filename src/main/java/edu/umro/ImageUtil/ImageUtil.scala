c996bd3471fd9404c0cbc93829eb513cb589887a
Jim Irrer <irrer@umich.edu>
2018-04-24 13:11:29 -0400

    Migrating prototype code to production.

--- src/main/java/edu/umro/ImageUtil/ImageUtil.scala
+++ src/main/java/edu/umro/ImageUtil/ImageUtil.scala
@@ -1,73 +1,74 @@
-package learn
+package edu.umro.ImageUtil
 
-import edu.umro.ImageUtil.FindWorstPixels
 import com.pixelmed.dicom.AttributeList
-import net.imglib2.img.Img
-import net.imglib2.`type`.numeric.integer.UnsignedShortType
 import io.scif.img.SCIFIOImgPlus
 import ij.gui.NewImage
-import ij.ImagePlus
-import ij.plugin.PGM_Reader
 import ij.process.ShortProcessor
 import ij.ImageStack
 import com.pixelmed.dicom.TagFromName
 import io.scif.img.ImgOpener
 import java.awt.image.BufferedImage
-import net.imglib2.util.Intervals
-import net.imglib2.Interval
 import net.imglib2.view.Views
 import java.awt.Color
 import net.imglib2.`type`.numeric.RealType
 import scala.collection.JavaConverters._
-import java.util.{ Iterator => JIterator }
 import java.io.File
 import javax.imageio.ImageIO
+import scala.Left
+import scala.Right
 
-object CallImglib2 {
+object ImageUtil {
 
-  def attributeListToImageStack(attributeList: AttributeList): Option[ImageStack] = {
-    val width = attributeList.get(TagFromName.Columns).getIntegerValues()(0)
-    val height = attributeList.get(TagFromName.Rows).getIntegerValues()(0)
-    val pixels = attributeList.getPixelData.getShortValues
-    val ip = new ShortProcessor(width, height, pixels, null)
-    val img = NewImage.createShortImage("hey", width, height, 1, 0)
-    val stack: ImageStack = new ImageStack(width, height);
-    stack.addSlice("hey", ip);
-    Some(stack)
-  }
+  private type T = RealType[_]
 
+  /**
+   * Read a DICOM file into an imglib2 object.
+   */
   def readDicomFile(dicomFileName: String): Either[String, SCIFIOImgPlus[_]] = {
     try {
       Right((new ImgOpener).openImgs(dicomFileName).get(0))
     } catch {
       case t: Throwable => {
         Left("Unexpected error: " + t.toString)
-        null
       }
     }
   }
 
-  private type T = RealType[_]
-
+  /**
+   * Container for a pair of min and max values.
+   */
   case class MinMax(min: Float, max: Float) {
     def this() = this(Float.MaxValue, Float.MinValue)
 
-    def extreme(v: Float) = {
+    def update(v: Float) = {
       new MinMax(Math.min(min, v), Math.max(max, v))
     }
   }
 
+  /**
+   * Find the minimum and maximum values of an image.
+   */
   def minMaxValuesOf(imgPlus: SCIFIOImgPlus[_]): MinMax = {
     val cursor = Views.iterable(imgPlus).cursor.asScala.asInstanceOf[Iterator[T]]
-    val minMax = cursor.foldLeft(new MinMax)((mm, v) => mm.extreme(v.getRealFloat))
+    val minMax = cursor.foldLeft(new MinMax)((mm, v) => mm.update(v.getRealFloat))
     minMax
   }
 
+  /**
+   * Convert an imglib2 image to an RGB BufferedImage.  Note that the brightness is based on
+   * the min and max pixel values for the image.  Normally DICOM image brightness is based on
+   * a series-wide basis as opposed to individual slices.
+   *
+   * @param imgPlus: Image to convert.
+   *
+   * @param color: Maximum color level for pixels.  Example: <code>Color.GREEN</code>.
+   */
   def imgPlusToBufferedImage(imgPlus: SCIFIOImgPlus[_], color: Color): BufferedImage = {
     val width = imgPlus.getImg.dimension(0).toInt
     val height = imgPlus.getImg.dimension(1).toInt
     val minMax = minMaxValuesOf(imgPlus)
 
+    // calculate a lookup table once
     val rgbTable = {
       val red = color.getRed
       val green = color.getGreen
@@ -83,7 +84,6 @@ object CallImglib2 {
             case _ if (i > 255) => 255
             case i => i
           }
-
           lev << shift
         }
 
@@ -95,6 +95,9 @@ object CallImglib2 {
 
     val ratio = 256 / (minMax.max - minMax.min)
 
+    /**
+     * Given a pixel level, return the RGB color.
+     */
     def levelToColor(level: Float): Int = {
       val i = (level * ratio).round.toInt
 
@@ -115,24 +118,6 @@ object CallImglib2 {
     }
 
     bufImage
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    val fileName = "src\\test\\resources\\TestFindWorstPixels.dcm"
-
-    val imgPlus = readDicomFile(fileName).right.get
-    val minMax = minMaxValuesOf(imgPlus)
-    println("minMax: " + minMax)
-
-    val bufImg = imgPlusToBufferedImage(imgPlus, Color.CYAN)
-    val pngName = "target\\foo.png"
-    val pngFile = new File(pngName)
-    pngFile.delete
-    ImageIO.write(bufImg, "png", pngFile)
-
-    println("done")
-
   }
 
 }
---
+++ src/test/java/edu/umro/ImageUtil/test/TestImageUtil.scala
@@ -0,0 +1,40 @@
+package edu.umro.ImageUtil
+
+import com.pixelmed.dicom.AttributeList
+import io.scif.img.SCIFIOImgPlus
+import ij.gui.NewImage
+import ij.process.ShortProcessor
+import ij.ImageStack
+import com.pixelmed.dicom.TagFromName
+import io.scif.img.ImgOpener
+import java.awt.image.BufferedImage
+import net.imglib2.view.Views
+import java.awt.Color
+import net.imglib2.`type`.numeric.RealType
+import scala.collection.JavaConverters._
+import java.io.File
+import javax.imageio.ImageIO
+import scala.Left
+import scala.Right
+
+object TestImageUtil {
+
+  def main(args: Array[String]): Unit = {
+
+    val fileName = "src\\test\\resources\\TestFindWorstPixels.dcm"
+
+    val imgPlus = ImageUtil.readDicomFile(fileName).right.get
+    val minMax = ImageUtil.minMaxValuesOf(imgPlus)
+    println("minMax: " + minMax)
+
+    val bufImg = ImageUtil.imgPlusToBufferedImage(imgPlus, Color.CYAN)
+    val pngName = "target\\foo.png"
+    val pngFile = new File(pngName)
+    pngFile.delete
+    ImageIO.write(bufImg, "png", pngFile)
+
+    println("done")
+
+  }
+
+}
