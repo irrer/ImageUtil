f26f67c5bc45dbc34e195f4b27428214ba2a9c85
Jim Irrer <irrer@umich.edu>
2019-07-02 18:27:19 -0400

    documenting edge location

---
+++ src/test/java/learn/LocateEdgeImageDoc.scala
@@ -0,0 +1,52 @@
+package learn
+
+import java.io.File
+import com.pixelmed.dicom.AttributeList
+import edu.umro.ImageUtil.DicomImage
+import java.awt.Color
+import javax.imageio.ImageIO
+import edu.umro.ImageUtil.ImageUtil
+import java.awt.Rectangle
+import org.opensourcephysics.numerics.CubicSpline
+
+object LocateEdgeImageDoc {
+
+  private val inDir = new File("""src\test\resources\LocateEdgeImageDoc""")
+
+  private def read(name: String): AttributeList = {
+    val file = new File(inDir, name)
+    val al = new AttributeList
+    al.read(file)
+    al
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val attrList = read("J10G0C270-6X.dcm")
+
+    val dicomImage = new DicomImage(attrList)
+    val x = 773
+    val y = 416
+    val width = 893
+    val height = 357
+    val edgeImage = dicomImage.getSubimage(new Rectangle(x, y, width, height))
+    val profile = edgeImage.columnSums.map(p => p.toDouble).toArray
+
+    val granularity = 10
+
+    //println("Integer values:\n" + (0 to profile.size by granularity).map(i => i + "," + profile(i)).mkString("\n"))
+    println("-------")
+
+    val cubicSpline = new CubicSpline(profile.indices.toArray.map(i => i.toDouble), profile)
+
+    def toGraph(x: Int) = {
+      val g2 = granularity / 2
+      val i = ((x - g2) / granularity) * granularity
+      println(x + "," + profile(i) + "," + cubicSpline.evaluate(x))
+    }
+
+    (0 to (profile.size)).map(x => toGraph(x))
+
+  }
+
+}
