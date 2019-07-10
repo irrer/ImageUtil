b8498934204346747615e0259d71623ae5dfca58
Jim Irrer <irrer@umich.edu>
2019-07-10 16:39:11 -0400

    working on CBCT

--- src/main/java/edu/umro/ImageUtil/DicomVolume.scala
+++ src/main/java/edu/umro/ImageUtil/DicomVolume.scala
@@ -35,25 +35,101 @@ case class DicomVolume(volume: Seq[DicomImage]) {
   }
 
   /**
+   * Get the value of a voxel.
+   */
+  def getXYZ(x: Int, y: Int, z: Int) = {
+    if (volume(z).get(x, y) == 0) // TODO rm
+      System.currentTimeMillis
+    volume(z).get(x, y)
+  }
+
+  /**
+   * Get the sum of a sub-volume.
+   */
+  private def sum(start: Point3i, size: Point3i): Float = {
+    val sum = (for (
+      x <- start.getX until (start.getX + size.getX);
+      y <- start.getY until (start.getY + size.getY);
+      z <- start.getZ until (start.getZ + size.getZ)
+    ) yield { getXYZ(x, y, z) }).sum
+    sum
+  }
+
+  /**
+   * Find the sub-volume of the given size that has the highest sum of voxel values.
+   *
+   * @param size Size of sub-volume
+   *
+   * @return Start of sub-volume (point closest to origin)
+   */
+  def getHighest(size: Point3i): Point3i = {
+    val vList = for (
+      x <- 0 until xSize - size.getX;
+      y <- 0 until ySize - size.getY;
+      z <- 0 until zSize - size.getZ
+    ) yield (new Point3i(x, y, z))
+
+    val max = vList.maxBy(v => sum(v, size))
+    //    val j = vList.map(v => sum(v, size)) // TODO rm
+    //    println("\n" + vList.map(v => sum(v, size).toInt + " " + v).mkString("\n")) // TODO rm
+    max
+  }
+
+  /**
    * Get the point of the volume that is the highest pixel intensity.  Result is in units of voxels.
    */
   def getMaxPoint: Point3d = {
+    Trace.trace
+    val xPlaneProfile = (0 until xSize).map(x => {
+      val k = for (y <- 0 until ySize; z <- 0 until zSize) yield (getXYZ(x, y, z))
+      k.sum
+    })
 
-    val xPlaneProfile = volume.map(img => img.rowSums).map(voxel => voxel.sum)
-    val yPlaneProfile = volume.map(img => img.columnSums).map(voxel => voxel.sum)
+    Trace.trace
+    val rowSums = volume.map(img => img.rowSums)
+    Trace.trace
+
+    //    val xPlaneProfile = {
+    //      def allXinY(y: Int) = {
+    //        (0 until zSize).map(z => rowSums(z)(y)).sum
+    //      }
+    //      val j = (0 until ySize).map(y => allXinY(y))
+    //      j
+    //    }
+
+    val yPlaneProfile = {
+      (0 until ySize).map(y => {
+        val k = for (x <- 0 until xSize; z <- 0 until zSize) yield (getXYZ(x, y, z))
+        k.sum
+      })
+    }
+    Trace.trace
+
+    //    val yPlaneProfile = {
+    //      val colSums = volume.map(img => img.columnSums)
+    //      def allYinX(x: Int) = {
+    //        (0 until zSize).map(z => rowSums(z)(x)).sum
+    //      }
+    //      val j = (0 until xSize).map(x => allYinX(x))
+    //      j
+    //    }
+
     val zPlaneProfile = volume.map(img => img.rowSums.sum)
+    Trace.trace
 
-    Trace.trace(xPlaneProfile)
-    Trace.trace(yPlaneProfile)
-    Trace.trace(zPlaneProfile)
+    Trace.trace("\n  " + xPlaneProfile.map(f => f.toInt).mkString(" "))
+    Trace.trace("\n  " + yPlaneProfile.map(f => f.toInt).mkString(" "))
+    Trace.trace("\n  " + zPlaneProfile.map(f => f.toInt).mkString(" "))
 
     val xMax = xPlaneProfile.indexOf(xPlaneProfile.max)
     val yMax = yPlaneProfile.indexOf(yPlaneProfile.max)
     val zMax = zPlaneProfile.indexOf(zPlaneProfile.max)
+    Trace.trace
 
     val xPosn = LocateMax.locateMax(xPlaneProfile)
     val yPosn = LocateMax.locateMax(yPlaneProfile)
     val zPosn = LocateMax.locateMax(zPlaneProfile)
+    Trace.trace
 
     new Point3d(xPosn, yPosn, zPosn)
   }
@@ -62,7 +138,11 @@ case class DicomVolume(volume: Seq[DicomImage]) {
 
 object DicomVolume {
 
-  private def slicePosition(attributeList: AttributeList): Double = attributeList.get(TagFromName.ImagePositionPatient).getDoubleValues()(2)
+  private def slicePosition(attributeList: AttributeList): Double = {
+    val j = attributeList.get(TagFromName.ImagePositionPatient).getDoubleValues()(2) // TODO rm
+    val s = attributeList.get(TagFromName.SOPInstanceUID).getSingleStringValueOrEmptyString // TODO rm
+    attributeList.get(TagFromName.ImagePositionPatient).getDoubleValues()(2)
+  }
 
   /**
    * Sort a list of DICOM images by their Z position.
@@ -74,7 +154,9 @@ object DicomVolume {
   /**
    * construct a volume from a list of attribute lists.
    */
-  def constructDicomVolume(alList: Seq[AttributeList]) = new DicomVolume(DicomVolume.sortDicomListByZ(alList.toSeq).map(al => new DicomImage(al)))
+  def constructDicomVolume(alList: Seq[AttributeList]) = {
+    new DicomVolume(DicomVolume.sortDicomListByZ(alList.toSeq).map(al => new DicomImage(al)))
+  }
 
   /**
    * Convert a Point3i to Point3d
--- src/main/java/edu/umro/ImageUtil/LocateMax.scala
+++ src/main/java/edu/umro/ImageUtil/LocateMax.scala
@@ -2,6 +2,7 @@ package edu.umro.ImageUtil
 
 import org.opensourcephysics.numerics.CubicSpline
 import edu.umro.ScalaUtil.Trace
+import java.security.InvalidParameterException
 
 /**
  * Edge location utilities.
@@ -13,31 +14,36 @@ object LocateMax {
    */
   private def toCubicSpline(data: Seq[Float]): CubicSpline = new CubicSpline(data.indices.toArray.map(s => s.toDouble), data.toArray.map(f => f.toDouble))
 
-  /** Perform the binary search up to this many times to be accurate to approximately this many bits of precision. */
-  private val maxSearchIteration = 128
-
-  private def binarySearch(lo: Double, hi: Double, prevLo: Double, prevHi: Double, iteration: Int, spline: CubicSpline): Double = {
-
-    // If iterating too long or there is no change, then use the result we have.
-    if ((iteration >= maxSearchIteration) || ((lo == prevLo) && (hi == prevHi))) {
-      (hi + lo) / 2
-    } else {
-
-      val count = 5 // evenly divide into this many intervals in the region of interest
-      val interval = (hi - lo) / count
-      val xList = (0 to count).map(i => lo + (i * interval))
-      val yList = xList.map(x => spline.evaluate(x))
-
-      // Contains peak if point on either side is lower.
-      def containsPeak(i: Int) = {
-        (yList(i - 1) <= yList(i)) && (yList(i) >= (yList(i + 1)))
-      }
-
-      val mid = (1 until count).filter(i => containsPeak(i)).head
-
-      binarySearch(xList(mid - 1), xList(mid + 1), lo, hi, iteration + 1, spline)
-    }
-  }
+  //  /** Perform the binary search up to this many times to be accurate to approximately this many bits of precision. */
+  //  private val maxSearchIteration = 128
+  //
+  //  private def binarySearch(lo: Double, hi: Double, prevLo: Double, prevHi: Double, iteration: Int, spline: CubicSpline): Double = {
+  //
+  //    // If iterating too long or there is no change, then use the result we have.
+  //    if ((iteration >= maxSearchIteration) || ((lo == prevLo) && (hi == prevHi))) {
+  //      (hi + lo) / 2
+  //    } else {
+  //
+  //      val count = 9 // evenly divide into this many intervals in the region of interest
+  //      val interval = (hi - lo) / count
+  //      val xList = (0 to count).map(i => lo + (i * interval))
+  //      val yList = xList.map(x => spline.evaluate(x))
+  //
+  //      // Contains peak if point on either side is lower.
+  //      def containsPeak(i: Int) = {
+  //        (yList(i - 1) <= yList(i)) && (yList(i) >= (yList(i + 1)))
+  //      }
+  //
+  //      val mid = {
+  //        val peak = (1 until count).filter(i => containsPeak(i)).headOption
+  //        if (peak.isEmpty)
+  //          throw new InvalidParameterException("Given range does not contain a peak value.  iteration: " + iteration + "    Evaluated: " + yList.mkString("  "))
+  //        peak.get
+  //      }
+  //
+  //      binarySearch(xList(mid - 1), xList(mid + 1), lo, hi, iteration + 1, spline)
+  //    }
+  //  }
 
   /**
    * Locate the maximum value to a precise degree.  The assumption made is that there is one point that is the
@@ -46,15 +52,26 @@ object LocateMax {
    */
   def locateMax(profile: Seq[Float]): Double = {
 
+    Trace.trace(profile.mkString("  "))
     val max = profile.max
+    Trace.trace
     val initial = profile.indexOf(max)
+    Trace.trace
 
     val lo = Math.max(0, initial - 2)
     val hi = Math.min(profile.size - 1, initial + 2)
+    Trace.trace
 
     val spline = toCubicSpline(profile)
+    Trace.trace
 
-    val result = binarySearch(lo, hi, lo - 1, hi + 1, 0, spline)
+    val res = 50000
+    val incD = 1 / res.toDouble
+    val bestI = (0 to res).toSeq.maxBy(i => spline.evaluate(lo + (i * incD)))
+
+    val result = lo + (bestI * incD)
+    //val result = binarySearch(lo, hi, lo - 1, hi + 1, 0, spline)
+    Trace.trace(result)
 
     result
   }
