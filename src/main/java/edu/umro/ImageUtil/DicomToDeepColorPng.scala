aebc90a1363fa11968b6d6cd57d784cc9eff879e
Jim Irrer <irrer@umich.edu>
2020-01-08 17:41:54 -0500

    added utility for making color images from DICOM

---
+++ src/main/java/edu/umro/ImageUtil/DicomToDeepColorPng.scala
@@ -0,0 +1,64 @@
+package edu.umro.ImageUtil
+
+import java.io.File
+import com.pixelmed.dicom.AttributeList
+import com.pixelmed.dicom.TagFromName
+
+object DicomToDeepColorPng {
+
+  /**
+   * Safely get a list of files in a directory.  On failure, return an empty list.
+   */
+  private def listFiles(dir: File): List[File] = {
+    try {
+      dir.listFiles.toList
+    } catch {
+      case t: Throwable => List[File]()
+    }
+  }
+
+  private def listRegularFiles(file: File): Seq[File] = {
+    if (file.isFile) Seq(file)
+    else {
+      val fList = listFiles(file)
+      fList.map(f => listRegularFiles(f)).flatten
+    }
+  }
+
+  /**
+   * Get the attribute list if this is a DICOM image file, otherwise return None.
+   */
+  private def getImageAl(file: File): Option[(File, AttributeList)] = {
+    try {
+      val al = new AttributeList
+      al.read(file)
+      if (al.get(TagFromName.PixelData) == null)
+        None
+      else
+        Some(file, al)
+    } catch {
+      case t: Throwable => None
+    }
+  }
+
+  private def toPng(file: File, al: AttributeList) = {
+    val di = new DicomImage(al)
+    val bufImage = di.toDeepColorBufferedImage(0.01)
+    val imageFile = {
+      val fileName = file.getName
+      val imageFileName = if (fileName.toLowerCase.endsWith(".dcm")) {
+        fileName.dropRight(4) + ".png"
+      } else
+        fileName + ".dcm"
+      new File(file.getParentFile, imageFileName)
+    }
+    imageFile.delete
+    println("Creating " + imageFile.getAbsolutePath)
+    ImageUtil.writePngFile(bufImage, imageFile)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val list = args.map(fileName => listRegularFiles(new File(fileName))).flatten.map(file => getImageAl(file)).flatten
+    list.map(fd => toPng(fd._1, fd._2))
+  }
+}
