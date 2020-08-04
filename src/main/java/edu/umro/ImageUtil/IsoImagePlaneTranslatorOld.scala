c7dca601087a11c4b8746d2099cf7b98d7797745
Jim Irrer <irrer@umich.edu>
2020-08-04 15:56:03 -0400

    revised IsoImagePlaneTranslator to use RTImagePosition if available

--- src/main/java/edu/umro/ImageUtil/IsoImagePlaneTranslatorOld.scala
+++ src/main/java/edu/umro/ImageUtil/IsoImagePlaneTranslatorOld.scala
@@ -4,6 +4,7 @@ import com.pixelmed.dicom.AttributeList
 import java.awt.geom.Point2D
 import com.pixelmed.dicom.AttributeTag
 import com.pixelmed.dicom.TagFromName
+//import org.aqa.webrun.phase2.Phase2Util
 
 /**
  * Support mapping points between the isoplane and the image plane.  Isoplane values are in mm with
@@ -11,7 +12,7 @@ import com.pixelmed.dicom.TagFromName
  * upper left corner of the image.
  */
 
-class IsoImagePlaneTranslator(al: AttributeList) {
+class IsoImagePlaneTranslatorOld(al: AttributeList) {
   private def dblOf(tag: AttributeTag): Double = al.get(tag).getDoubleValues.head
   private def intOf(tag: AttributeTag): Int = al.get(tag).getIntegerValues.head
 
@@ -19,30 +20,19 @@ class IsoImagePlaneTranslator(al: AttributeList) {
   val width = intOf(TagFromName.Columns)
   val height = intOf(TagFromName.Rows)
 
+  // Image center in pixels
+  val imageCenter = new Point2D.Double(((width - 1.0) / 2), ((height - 1.0) / 2))
+
+  val beamExpansionRatio = dblOf(TagFromName.RTImageSID) / dblOf(TagFromName.RadiationMachineSAD)
+
   private val ImagePlanePixelSpacing = al.get(TagFromName.ImagePlanePixelSpacing).getDoubleValues
 
   val pixelSizeX = ImagePlanePixelSpacing(0)
   val pixelSizeY = ImagePlanePixelSpacing(1)
 
-  val beamExpansionRatio = dblOf(TagFromName.RTImageSID) / dblOf(TagFromName.RadiationMachineSAD)
-
   // Multiply this value by a measurement in mm in the isoplane to get the corresponding value in pixels in the image plane.
   private val expansionFactorX = beamExpansionRatio / pixelSizeX
   private val expansionFactorY = beamExpansionRatio / pixelSizeY
-
-  // Upper left corner of image in isoplane coordinates
-  private val imageUpperLeft: Point2D.Double = {
-    val RTImagePosition = al.get(TagFromName.RTImagePosition)
-    if (RTImagePosition == null) {
-      val x = -(((width - 1.0) / 2) * pixelSizeX) / beamExpansionRatio
-      val y = -(((height - 1.0) / 2) * pixelSizeY) / beamExpansionRatio
-      new Point2D.Double(x, y)
-    } else {
-      val x = RTImagePosition.getDoubleValues()(0) / beamExpansionRatio
-      val y = (-(RTImagePosition.getDoubleValues()(1))) / beamExpansionRatio
-      new Point2D.Double(x, y)
-    }
-  }
 
   /** convert x distance in mm from isoplane to pixel plane. */
   def iso2PixDistX(x: Double) = x * expansionFactorX
@@ -50,9 +40,9 @@ class IsoImagePlaneTranslator(al: AttributeList) {
   def iso2PixDistY(y: Double) = y * expansionFactorY
 
   /** convert x coordinate from isoplane in mm to pixel plane. */
-  def iso2PixCoordX(x: Double) = (x - imageUpperLeft.getX) * expansionFactorX
+  def iso2PixCoordX(x: Double) = (x * expansionFactorX) + imageCenter.getX
   /** convert y coordinate from isoplane in mm to pixel plane. */
-  def iso2PixCoordY(y: Double) = (y - imageUpperLeft.getY) * expansionFactorY
+  def iso2PixCoordY(y: Double) = (y * expansionFactorY) + imageCenter.getY
 
   /** convert a coordinate pair from isoplane in mm to pixel plane. */
   def iso2Pix(x: Double, y: Double): Point2D.Double = new Point2D.Double(iso2PixCoordX(x), iso2PixCoordY(y))
@@ -65,9 +55,9 @@ class IsoImagePlaneTranslator(al: AttributeList) {
   def pix2IsoDistY(y: Double) = y / expansionFactorY
 
   /** convert x coordinate from pixel plane to isoplane in mm. */
-  def pix2IsoCoordX(x: Double) = (x / expansionFactorX) + imageUpperLeft.getX
+  def pix2IsoCoordX(x: Double) = (x - imageCenter.getX) / expansionFactorX
   /** convert y coordinate from pixel plane to isoplane in mm. */
-  def pix2IsoCoordY(y: Double) = (y / expansionFactorY) + imageUpperLeft.getY
+  def pix2IsoCoordY(y: Double) = (y - imageCenter.getY) / expansionFactorY
 
   /** convert a coordinate pair from pixel plane to isoplane in mm. */
   def pix2Iso(x: Double, y: Double): Point2D.Double = new Point2D.Double(pix2IsoCoordX(x), pix2IsoCoordY(y))
@@ -93,22 +83,6 @@ class IsoImagePlaneTranslator(al: AttributeList) {
       "    height: " + height + ", " + "    width: " + width + ", " +
       "    ImagePlanePixelSpacing: " + pixelSizeX.formatted("%7.5f") + ", " + pixelSizeY.formatted("%7.5f")
   }
-
-  val validRTImagePosition: Boolean = {
-    val RTImagePosition = al.get(TagFromName.RTImagePosition)
-    if (RTImagePosition == null)
-      true
-    else {
-      val min = 0.00000001
-      val xIso = RTImagePosition.getDoubleValues()(0)
-      val yIso = -(RTImagePosition.getDoubleValues()(1))
-      val pix = iso2Pix(xIso, yIso)
-      val valid = (pix.getX.abs < min) && (pix.getY.abs < min)
-      valid
-    }
-  }
-
-  // if (!validRTImagePosition) throw new RuntimeException("RTImagePosition is inconsistent with expected values"
 
 }
 
---
+++ src/test/java/edu/umro/ImageUtil/test/TestIsoImagePlaneTranslator.scala
@@ -0,0 +1,56 @@
+package edu.umro.ImageUtil.test;
+
+import edu.umro.ScalaUtil.Trace
+import java.io.File
+import com.pixelmed.dicom.AttributeList
+import edu.umro.ImageUtil.DicomImage
+import java.awt.Rectangle
+import edu.umro.util.Utility
+import edu.umro.ImageUtil.ImageUtil
+import edu.umro.ImageUtil.IsoImagePlaneTranslator
+import edu.umro.ImageUtil.IsoImagePlaneTranslatorOld
+
+object TestIsoImagePlaneTranslator {
+
+  val inDir = new File("""src\test\resources\TestIsoImagePlaneTranslator""")
+
+  def check(name: String, cur: Double, old: Double) {
+    val diff = cur - old
+    def fmt(d: Double) = d.formatted("%14.8f")
+    println(name.formatted("%-16s") + " cur: " + fmt(cur) + "     old: " + fmt(old) + "    diff: " + fmt(diff))
+    if (diff.abs > 0.00001) {
+      Thread.sleep(100)
+      throw new RuntimeException("diff is too large")
+    }
+  }
+
+  def testFile(file: File) = {
+    val al = new AttributeList
+    al.read(file)
+    println("\nTesting file " + file.getAbsolutePath)
+
+    val ipt = new IsoImagePlaneTranslator(al)
+    val iptOld = new IsoImagePlaneTranslatorOld(al)
+
+    check("iso2PixCoordX", ipt.iso2PixCoordX(0), iptOld.iso2PixCoordX(0))
+
+    for (v <- (-10000 to 10000)) {
+      val d = v / 10.0
+      check("iso2PixDistX", ipt.iso2PixDistX(d), iptOld.iso2PixDistX(d))
+      check("iso2PixCoordX", ipt.iso2PixCoordX(d), iptOld.iso2PixCoordX(d))
+      check("iso2PixDistY", ipt.iso2PixDistY(d), iptOld.iso2PixDistY(d))
+      check("iso2PixCoordY", ipt.iso2PixCoordY(d), iptOld.iso2PixCoordY(d))
+
+      check("pix2IsoDistX", ipt.pix2IsoDistX(d), iptOld.pix2IsoDistX(d))
+      check("pix2IsoCoordX", ipt.pix2IsoCoordX(d), iptOld.pix2IsoCoordX(d))
+      check("pix2IsoDistY", ipt.pix2IsoDistY(d), iptOld.pix2IsoDistY(d))
+      check("pix2IsoCoordY", ipt.pix2IsoCoordY(d), iptOld.pix2IsoCoordY(d))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    inDir.listFiles.toSeq.map(f => testFile(f))
+    println("all tests passed")
+  }
+
+}
