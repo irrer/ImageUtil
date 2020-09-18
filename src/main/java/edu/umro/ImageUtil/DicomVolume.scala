7193b3775b8ff936e23f09c5533900d6bd1da70e
Jim Irrer <irrer@umich.edu>
2020-09-18 19:59:06 -0400

    change from array to point

--- src/main/java/edu/umro/ImageUtil/DicomVolume.scala
+++ src/main/java/edu/umro/ImageUtil/DicomVolume.scala
@@ -17,7 +17,7 @@ case class DicomVolume(volume: Seq[DicomImage]) {
   val ySize = volume.head.height
   val zSize = volume.size
 
-  val volSize = Seq(xSize, ySize, zSize)
+  val volSize = new Point3i(xSize, ySize, zSize)
 
   /**
    * Get a sub-volume of the given volume given the min and max XYZ coordinates.
@@ -54,6 +54,51 @@ case class DicomVolume(volume: Seq[DicomImage]) {
   }
 
   /**
+   * Get the sum of each line of voxels in the X axis.  In other words, all
+   * voxels with the same y and z coordinates are summed.  This is akin to
+   * creating a 2 dimensional profile seen from the X axis.
+   */
+  def sumX: DicomImage = {
+    def getRow(yy: Int): IndexedSeq[Float] = {
+      for (z <- 0 until zSize) yield { (0 until xSize).map(x => getXYZ(x, yy, z)).sum }
+    }
+
+    val matrix = for (y <- 0 until ySize) yield getRow(y)
+
+    new DicomImage(matrix)
+  }
+
+  /**
+   * Get the sum of each line of voxels in the Y axis.  In other words, all
+   * voxels with the same x and z coordinates are summed.  This is akin to
+   * creating a 2 dimensional profile seen from the Y axis.
+   */
+  def sumY: DicomImage = {
+    def getRow(zz: Int): IndexedSeq[Float] = {
+      for (x <- 0 until xSize) yield { (0 until ySize).map(y => getXYZ(x, y, zz)).sum }
+    }
+
+    val matrix = for (z <- 0 until zSize) yield getRow(z)
+
+    new DicomImage(matrix)
+  }
+
+  /**
+   * Get the sum of each line of voxels in the Z axis.  In other words, all
+   * voxels with the same x and y coordinates are summed.  This is akin to
+   * creating a 2 dimensional profile seen from the Z axis.
+   */
+  def sumZ: DicomImage = {
+    def getRow(yy: Int): IndexedSeq[Float] = {
+      for (x <- 0 until xSize) yield { (0 until zSize).map(z => getXYZ(x, yy, z)).sum }
+    }
+
+    val matrix = for (y <- 0 until ySize) yield getRow(y)
+
+    new DicomImage(matrix)
+  }
+
+  /**
    * Find the sub-volume of the given size that has the highest sum of voxel values.  In
    * other words, look for bright blobs of a given size.
    *
@@ -73,7 +118,9 @@ case class DicomVolume(volume: Seq[DicomImage]) {
   }
 
   /**
-   * Get the point of the volume that is the highest pixel intensity.  Result is in units of voxels.
+   * Get the point of the volume that is the center of the highest pixel intensity.  This is done by getting
+   * the profile in each of the three axis and finding the maximum point on the cubic spline curve.  Result
+   * is in units of voxels.
    *
    * If the standard deviation in any of the 3 dimensions is below the given minimum standard deviation, then
    * return None, indicating that the maximum point found was not sufficiently distinguishable from
