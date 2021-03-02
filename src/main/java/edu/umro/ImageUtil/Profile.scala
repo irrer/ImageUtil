b64e478022e0e17af4c99384e3bed2b52f1c6fdc
Jim Irrer <irrer@umich.edu>
2021-03-02 08:00:12 -0500

    do not add non-open source, removed unused imports

---
+++ src/main/java/edu/umro/ImageUtil/Profile.scala
@@ -0,0 +1,69 @@
+package edu.umro.ImageUtil
+
+import org.opensourcephysics.numerics.CubicSpline
+
+/**
+ * Edge location utilities.
+ */
+case class Profile(data: Seq[Float]) {
+
+  /**
+   * Convert a list to a cubic spline
+   */
+  val cubicSpline = new CubicSpline(data.indices.toArray.map(s => s.toDouble), data.toArray.map(f => f.toDouble))
+
+  val mean: Float = data.sum / data.size
+
+  val standardDeviation = ImageUtil.stdDev(data)
+
+  /** Maximum number of iterations to approximate answer. */
+  private val minIteration = 10
+
+  /** Maximum number of iterations to approximate answer. */
+  private val maxIteration = 20
+
+  /**
+   * Locate the maximum value to a precise degree.  The assumption made is that there is one point that is the
+   * highest.  The algorithm might fail if given a profile with a series of peaks that are similar in height.
+   *
+   *  @param profile List of values describing profile that contains a maximum.
+   */
+  val max: Double = {
+
+    case class XY(x: Double, y: Double) {
+      override def toString: String = {
+        x.formatted("%20.14f") + ", " + y.formatted("%20.14f")
+      }
+    }
+
+    // Largest possible answer (end of array).  Never return a value larger than this.  The lower limit is always 0.
+    val upperLimit = data.size - 1.0
+
+    val divisions = 10
+
+    def approximate(xyList: IndexedSeq[XY], iteration: Int, prevX: Double): Double = {
+      val best = xyList.maxBy(_.y)
+      //Trace.trace("iteration: " + iteration.formatted("%3d") + "    best: " + best.x + "  " + best.y)
+      if ((iteration >= maxIteration) ||
+        ((best.x == prevX) && (iteration > minIteration)))
+        best.x
+      else {
+        val prevIncr = xyList(1).x - xyList.head.x
+        val lo = Math.max(best.x - prevIncr, 0.0)
+        val hi = Math.min(best.x + prevIncr, upperLimit)
+        val incr = (hi - lo) / divisions
+
+        val newList = (0 to divisions).map(i => new XY(((i * incr) + lo), cubicSpline.evaluate((i * incr) + lo)))
+        //Trace.trace("\n    " + newList.mkString("\n    "))
+        approximate(newList, iteration + 1, best.x)
+      }
+    }
+
+    val incr = 1.0 / divisions
+    val list = (0 to ((data.size - 1) * divisions)).map(i => new XY(i * incr, cubicSpline.evaluate(i * incr)))
+    val result = approximate(list, 0, -1)
+
+    result
+  }
+
+}
